f2e0e13329b912c2c9ac3e41217d0a31bfecd3a6
jperomingan@startechup.com <jperomingan@startechup.com>
2020-12-03 16:04:58 +0800

    add absolute imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Header from './components/Header';
-import SignIn from './views/SignIn';
-import SignUp from './views/SignUp';
-import NGOList from './views/NGOList';
+import Header from 'components/Header';
+import SignIn from 'views/SignIn';
+import SignUp from 'views/SignUp';
+import NGOList from 'views/NGOList';
 
 function App() {
   return (
